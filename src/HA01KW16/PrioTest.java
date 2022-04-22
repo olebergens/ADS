f59e8411aaead4553860561b481379fb544e55fb
olebergens <infiziert.1337@gmail.com>
2022-04-22 13:58:01 +0200

    added stid

--- src/HA01KW16/PrioTest.java
+++ src/HA01KW16/PrioTest.java
@@ -1,5 +1,11 @@
 package HA01KW16;
 
+/**
+ * @AUTHOR: Ole Bergens, 221200097
+ * @AUTHOR: Blazej Schott, 221200610
+ * @AUTHOR: Antonin Gräser, 221201792
+ * @AUTHOR: Nils Martin, 221202136
+ */
 public class PrioTest {
 
     PQueue pqTest;
