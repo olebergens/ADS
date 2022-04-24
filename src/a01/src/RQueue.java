2028973fb86cb20616b2b31b96c4fbd7e7346142
Antonin <antonin.graeser@gmail.com>
2022-04-24 23:16:54 +0200

    amenakoi

---
+++ src/a01/src/RQueue.java
@@ -0,0 +1,58 @@
+public class RQueue {
+	
+	public int length=0;
+	
+	public RQueue() {
+		this.head = null;
+		this.errorel = new Element();
+		
+	Element head;
+	
+	public int length=0;
+	
+	public RQueue() {
+		this.head = null;	
+	}
+	
+	public RQueue rqempty() {		//Durch Konstruktorimplementierung überflüssig
+		return new RQueue();
+	}
+	
+	public RQueue rqinsert(int element, int prio) {
+		Element insertion;
+		insertion.prio = prio;
+		insertion.val = element;
+		insertion.next = this.head;
+		this.head = insertion;
+		this.length++;
+	}
+	
+	public Element rqfront() {
+		 Element tmp = this.head;
+		 if(head == null) {
+			 tmp = errorel;
+			}else {
+			if(head.next == null) {
+				tmp = this.head;
+			}else {
+
+		 Element runner = this.head.next;
+		 int i = 0;
+		 
+		 while(i < this.length) {
+			 if(tmp.prio <= runner.prio) {
+				 tmp = runner;
+				 i++;
+			 }
+		 }
+	}
+			}
+		 return tmp;
+	}
+	
+	public RQueue rqremove() {
+		
+	}
+	}
+=======
+}
