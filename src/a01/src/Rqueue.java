d98e65ef46c9d38cf1474d29a79441f3094de6f6
Antonin <antonin.graeser@gmail.com>
2022-04-24 19:22:06 +0200

    kafuha

---
+++ src/a01/src/Rqueue.java
@@ -0,0 +1,18 @@
+public class RQueue {
+	
+	Element Null;
+	
+	public int length=0;
+	
+	public RQueue() {
+		this.Null = null;	
+	}
+	
+	public RQueue rqempty() {		//Durch Konstruktorimplementierung überflüssig
+		return new RQueue();
+	}
+	
+	public RQueue rqinsert(Element element, int prio) {
+		element.prio
+	}
+}
