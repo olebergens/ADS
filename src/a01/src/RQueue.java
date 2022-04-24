20b5b80def56146bc7d552d2332b469e8646abb0
Antonin <antonin.graeser@gmail.com>
2022-04-24 23:30:42 +0200

    amenakoi

--- src/a01/src/RQueue.java
+++ src/a01/src/RQueue.java
@@ -1,3 +1,12 @@
+
+/**
+ * @AUTHOR: Ole Bergens, 221200097
+ * @AUTHOR: Blazej Schott, 221200610
+ * @AUTHOR: Antonin Gräser, 221201792
+ * @AUTHOR: Nils Martin, 221202136
+ */
+
+
 public class RQueue {
 	
 	public int length=0;
