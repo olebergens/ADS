f203b396b036824c6b550233a46a486eacbbb1cd
Zutxka <blazejschott@gmail.com>
2022-05-02 23:17:18 +0200

    SearchSort update

--- src/a02/src/SearchSort.java
+++ src/a02/src/SearchSort.java
@@ -3,6 +3,17 @@ package a02.src;
 public class SearchSort extends SortingAlgorithm {
 
     public void sort(int[] inputArray) {
-        
+        int[] arr = new int[inputArray.length];
+        int[] tmp = new int[1];
+        tmp[0] = inputArray[0];
+        for(int i = 0; i < inputArray.length - 1; i++){
+            for (int j = i+1; j < inputArray.length - 2; j++){
+                if((tmp[0] < inputArray[i]) == false){
+                    break;
+                }
+                else arr[i] = tmp[0];
+            tmp[0] =  inputArray[i];
+        }
     }
+}
 }
