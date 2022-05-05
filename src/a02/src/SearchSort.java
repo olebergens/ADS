4d5ff0622daf13afd14b4774df3cf75350b0f645
Zutxka <blazejschott@gmail.com>
2022-05-05 14:24:02 +0200

    SearchSort final

--- src/a02/src/SearchSort.java
+++ src/a02/src/SearchSort.java
@@ -1,19 +1,28 @@
+/**
+ * @AUTHOR: Ole Bergens, 221200097
+ * @AUTHOR: Blazej Schott, 221200610
+ * @AUTHOR: Antonin Gräser, 221201792
+ * @AUTHOR: Nils Martin, 221202136
+ */
+
 package a02.src;
 
 public class SearchSort extends SortingAlgorithm {
-
-    public void sort(int[] inputArray) {
-        int[] arr = new int[inputArray.length];
-        int[] tmp = new int[1];
-        tmp[0] = inputArray[0];
-        for (int i = 1; i < inputArray.length - 1; i++) {
-            for (int j = i + 1; j < inputArray.length - 2; j++) {
-                if ((tmp[0] < inputArray[j]) == false) {
-                    break; //Funktioniert nicht so 
-                } else
-                    arr[i] = tmp[0];
-            }
-            tmp[0] = inputArray[i];
+//Sucht das kleinste Element im Array und speichert sie dann in pos.
+//Sollte es das kleinste Element sein, dann fügt sie das kleinste Element(input[pos]) an der i-te Stelle
+    @Override
+    public void sort(int[] input) {
+        int tmp;
+        int pos;
+        for (int i = 0; i <= input.length - 2; i++) {
+            pos = i;
+            for (int j = i + 1; j <= input.length - 1; j++) {
+                if ((input[j] > input[pos]) == false)
+                    pos = j;
+                }
+            tmp = input[i]; 
+            input[i] = input[pos];
+            input[pos] = tmp;
         }
     }
 }
