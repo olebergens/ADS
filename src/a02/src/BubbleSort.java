00847d58072cc63a064f21211a7b84ca5b56d66c
Antonin <antonin.graeser@gmail.com>
2022-05-08 15:49:47 +0200

    okidoki

--- src/a02/src/BubbleSort.java
+++ src/a02/src/BubbleSort.java
@@ -4,3 +4,25 @@
  * @AUTHOR: Antonin Gräser, 221201792
  * @AUTHOR: Nils Martin, 221202136
  */
+
+package a02.src;
+
+public class BubbleSort extends SortingAlgorithm {
+//Sucht das kleinste Element im Array und speichert sie dann in pos.
+//Sollte es das kleinste Element sein, dann fügt sie das kleinste Element(input[pos]) an der i-te Stelle
+    public void sort(int[] input) {
+        int fin,tmp;
+        while(fin==0) {
+        	fin = 1;
+        	for(int i = 0; i < input.length; i++) {
+        		if(input[i] < input[i+1]) {
+        			fin = 0;
+        			tmp = input[i];
+        			input[i] = input[i+1];
+        			input[i+1] = tmp;
+        		}
+        	}
+        
+        }
+    }
+}
