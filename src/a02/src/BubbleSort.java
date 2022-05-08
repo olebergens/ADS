05570366377359a382947866f2d168ee9011d904
Ole Bergens <infiziert.1337@gmail.com>
2022-05-08 16:09:53 +0200

    push bis ans limit

--- src/a02/src/BubbleSort.java
+++ src/a02/src/BubbleSort.java
@@ -8,22 +8,23 @@
 package a02.src;
 
 public class BubbleSort extends SortingAlgorithm {
-//Sucht das kleinste Element im Array und speichert sie dann in pos.
-//Sollte es das kleinste Element sein, dann fügt sie das kleinste Element(input[pos]) an der i-te Stelle
-	@Override
+
+    /**Sucht das kleinste Element im Array und speichert sie dann in pos.
+    Sollte es das kleinste Element sein, dann fügt sie das kleinste Element(input[pos]) an der i-te Stelle **/
+    @Override
     public void sort(int[] input) {
-        int fin,tmp;
-        while(fin==0) {
-        	fin = 1;
-        	for(int i = 0; i < input.length; i++) {
-        		if(input[i] < input[i+1]) {
-        			fin = 0;
-        			tmp = input[i];
-        			input[i] = input[i+1];
-        			input[i+1] = tmp;
-        		}
-        	}
-        
+        int fin = 0, tmp;
+        while (fin == 0) {
+            fin = 1;
+            for (int i = 0; i < input.length - 1; i++) {
+                if (input[i] < input[i + 1]) {
+                    fin = 0;
+                    tmp = input[i];
+                    input[i] = input[i + 1];
+                    input[i + 1] = tmp;
+                }
+            }
+
         }
     }
 }
