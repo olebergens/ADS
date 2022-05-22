d8a7d86f571eecaaa56793e4e351f81e7c4a0681
Antonin <antonin.graeser@gmail.com>
2022-05-22 19:04:39 +0200

    lol

---
+++ src/a03/src/SimpleQuicksort.java
@@ -0,0 +1,15 @@
+package a03.src;
+
+/**
+ * @AUTHOR: Ole Bergens, 221200097
+ * @AUTHOR: Blazej Schott, 221200610
+ * @AUTHOR: Antonin Gräser, 221201792
+ * @AUTHOR: Nils Martin, 221202136
+ */
+public class SimpleQuicksort extends AbsQuicksort{
+	@Override
+    public void quicksort(int[] arr) {
+    	
+    }
+    
+}
