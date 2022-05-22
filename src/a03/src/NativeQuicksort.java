df76e5ff5fe7b467f484fe7b7218fa72ce09385e
Antonin <antonin.graeser@gmail.com>
2022-05-22 19:31:03 +0200

    lol

--- src/a03/src/NativeQuicksort.java
+++ src/a03/src/NativeQuicksort.java
@@ -9,7 +9,32 @@ package a03.src;
 public class NativeQuicksort extends AbsQuicksort{
 	@Override
     public void quicksort(int[] arr) {
-    	
+        quicksort(arr, 0, arr.length - 1);
     }
-    
+	
+	int part(int[] arr, int left, int right) {
+		int pivot = arr[left];
+		int i = left;		
+		for(int j = left; j <= right - 1; j++) {
+			if(arr[j] < pivot) {
+				int tmp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = tmp;
+				i++;
+			}
+		}
+		int tmp = arr[i];
+		arr[i] = arr[right];
+		arr[right] = tmp;
+		return(i);
+	}
+	
+	
+    public static void quicksort(int[] arr, int left, int right) {
+    	if(left<right) {
+    	int part = part(arr, 0, arr.length - 1);
+    	quicksort(arr, left, part - 1);
+        quicksort(arr, part + 1, right);
+    }
+    }
 }
