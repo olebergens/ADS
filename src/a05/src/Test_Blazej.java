b7c9b4c7f202e36f2d38a4bb1f5775e139b08aeb
Zutxka <blazejschott@gmail.com>
2022-06-25 20:47:53 +0200

    Test

---
+++ src/a05/src/Test_Blazej.java
@@ -0,0 +1,14 @@
+package a05.src;
+
+import java.util.Scanner;
+
+public class Test_Blazej {
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Nutzer eingabe:");
+        String eingabe = in.nextLine();
+        System.out.println("Antonin kann kein Java " + eingabe);
+    }
+
+}
